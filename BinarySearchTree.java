978afbfea3504b856052e620067992d1eeca799d
CodeProdigy88 <cameronmckay88@gmail.com>
2026-06-29 20:45:49 -0600

    Add files via upload

---
+++ BinarySearchTree.java
@@ -0,0 +1,69 @@
+package assign08;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class BinarySearchTree<Type extends Comparable<? super Type>> implements SortedSet<Type> {
+
+	@Override
+	public Iterator<Type> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(Type item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void addAll(Collection<? extends Type> items) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean contains(Type item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(Type item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Type first() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type last() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
